524cddac7d559e5b94f08a9083cce54ac8c7b83e
dohyean <65577625+dohyean@users.noreply.github.com>
2025-02-12 19:22:10 +0900

    2025-02-12
    - dir_entropy 일부 데이터 추출 안 된 현상 해결 완료
    - readme 파일 변경

--- function/extract_feature/function/extract_dir_entropy.py
+++ function/extract_feature/function/extract_dir_entropy.py
@@ -1,12 +1,26 @@
 import pefile
+from value.extract_feature.dir_entropy import directory_names
 
 # dir 엔트로피 추출 
 def ExtractDirEntropy(file_path, list_value):
     pe = pefile.PE(file_path)
-    for directory in pe.OPTIONAL_HEADER.DATA_DIRECTORY:
-        value = False
-        if hasattr(pe, directory.name.replace("IMAGE_", "")):
-             value = True
-        list_value.append(value)        
-        list_value.append(hex(directory.VirtualAddress))        
-        list_value.append(directory.Size)        
+    
+    for directory_name in directory_names:
+        full_directory_name = f"IMAGE_DIRECTORY_ENTRY_{directory_name}"
+
+        directory_found = False
+        for directory in pe.OPTIONAL_HEADER.DATA_DIRECTORY:
+            if directory.name == full_directory_name:
+                directory_found = True
+                
+                value = hasattr(pe, directory.name.replace("IMAGE_", ""))
+                list_value.append(value)
+                list_value.append(hex(directory.VirtualAddress))
+                list_value.append(directory.Size)
+                break
+        
+        if not directory_found:
+            list_value.append(False) 
+            list_value.append(hex(0))   
+            list_value.append(0)   
+            print(f"Directory {full_directory_name} not found.")
--- value/extract_exe_file/extract_exe_file_README.md
+++
@@ -1,2 +0,0 @@
-# extract_exe_file
-`extract_exe_file`는 extract_feature에서 특징이 추출될 파일이 저장될 디렉토리입니다.
